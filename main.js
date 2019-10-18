10a9c829a8428c626b5a015382b60ec28bbc1333
allegro62 <allegro62@users.noreply.github.com>
2019-10-19 03:25:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello NodeApp!! --- Updated version\n");
+  response.end("Hello NodeApp!! --- Updated new version\n");
 });
 
 // listen on localhost:8000
